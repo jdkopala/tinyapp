dc4edf6a4a6648d0e112262354f45d15810f7456
Jesse Kopala <jdkopala@gmail.com>
2022-06-16 14:15:14 -0600

    Implemented cookie-session to use encrypted cookies

--- express_server.js
+++ express_server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const cookieSession = require('cookie-session');
 const { restart } = require('nodemon');
 const bcrypt = require('bcryptjs');
 const PORT = 8080;
@@ -10,16 +10,7 @@ app.set("view engine", "ejs");
 
 const users = {};
 
-const urlDatabase = {
-  "B2xVn2": {
-    "longURL": "http://www.lighthouselabs.ca", 
-    "userId": "GtNXXT"
-  },
-  "9sm5xK": {
-    "longURL": "http://www.google.com", 
-    "userId": "GtNXXT"
-  }
-};
+const urlDatabase = {};
 
 const urlsForUser = (id) => {
   let filterData = {};
@@ -57,7 +48,11 @@ const generateRandomString = () => {
 //
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
+app.use(cookieSession( {
+  name: "tinyApp",
+  keys: ['Ususally', 'These', 'Are', 'a', 'secret'],
+  maxAge: 24 * 60 * 60 * 1000 // This will make the cookie last 24hrs
+}));
 
 //
 // Routes: GET
@@ -74,18 +69,18 @@ app.get("/u/:shortURL", (req, res) => {
       const longURL = urlDatabase[shortURL]["longURL"];
       res.redirect(longURL);
     } else {
-      res.status(404).send("Tiny URL does not exist. Try another");
+      res.status(404).send("Tiny URL does not exist.");
     }
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, userUrls: urlsForUser(req.cookies["userId"]), user_id: req.cookies["userId"], users };
+  const templateVars = { urls: urlDatabase, userUrls: urlsForUser(req.session.userId), user_id: req.session.userId, users };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { urls: urlDatabase, userUrls: urlsForUser(req.cookies["userId"]), user_id: req.cookies["userId"], users };
-  if (!req.cookies["userId"]) {
+  const templateVars = { urls: urlDatabase, userUrls: urlsForUser(req.session.userId), user_id: req.session.userId, users };
+  if (!req.session.userId) {
     res.redirect("/login");
   } else {
     res.render("urls_new", templateVars);
@@ -93,7 +88,7 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, urls: urlDatabase, userUrls: urlsForUser(req.cookies["userId"]), user_id: req.cookies["userId"], users };
+  const templateVars = { shortURL: req.params.shortURL, urls: urlDatabase, userUrls: urlsForUser(req.session.userId), user_id: req.session.userId, users };
   res.render("urls_show", templateVars);
 });
 
@@ -106,8 +101,8 @@ app.get("/users.json", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
-  const templateVars = { urls: urlDatabase, user_id: req.cookies["userId"], users };
-  if (req.cookies["userId"]) {
+  const templateVars = { urls: urlDatabase, user_id: req.session.userId, users };
+  if (req.session.userId) {
     res.redirect("/urls");
   } else {
     res.render("user_reg", templateVars);
@@ -115,8 +110,8 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/login", (req, res) => {
-  const templateVars = { urls: urlDatabase, user_id: req.cookies["userId"], users };
-  if (req.cookies["userId"]) {
+  const templateVars = { urls: urlDatabase, user_id: req.session.userId, users };
+  if (req.session.userId) {
     res.redirect("/urls");
   } else {
     res.render("user_login", templateVars);
@@ -128,21 +123,21 @@ app.get("/login", (req, res) => {
 //
 
 app.post("/urls", (req, res) => {
-  if (!req.cookies["userId"]) {
+  if (!req.session.userId) {
     res.status(400).send("Only registered users may create tiny URLs, please log in");
   } else {
     const shortURL = generateRandomString();
     const longURL = req.body.longURL;
     urlDatabase[shortURL] = {
       longURL: longURL,
-      userId: req.cookies["userId"]
+      userId: req.session.userId
     };
     res.redirect(`/urls/${shortURL}`);
   }
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-  if (!req.cookies["userId"]) {
+  if (!req.session.userId) {
     res.status(401).send("Only registered users may delete tiny URLs, please log in")
   } else {
     delete urlDatabase[req.params.shortURL];
@@ -153,12 +148,12 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = req.body.longURL;
-  if (!req.cookies["userId"]) {
+  if (!req.session.userId) {
     res.status(401).send("Only registered users may edit tiny URLs, please log in")
   } else {
     urlDatabase[shortURL] = urlDatabase[shortURL] = {
         longURL: longURL,
-        userId: req.cookies["userId"]
+        userId: req.session.userId
       };;
     res.redirect("/urls");
   }
@@ -180,7 +175,8 @@ app.post("/register", (req, res) => {
       email: newUserEmail,
       password: hashedPassword
     };
-    res.cookie("userId", newUserId);
+    // res.cookie("userId", newUserId);
+    req.session.userId = newUserId;
     res.redirect("/urls");
   }
 });
@@ -189,13 +185,14 @@ app.post("/login", (req, res) => {
   const candidateUserEmail = req.body.email;
   const candidatePassword = req.body.password;
   let user = checkUserEmails(candidateUserEmail);
-  let hashCheck = bcrypt.compareSync(candidatePassword, user.password);
   if (!user) {
     res.status(403).send("That user does not exist");
   } else {
+    let hashCheck = bcrypt.compareSync(candidatePassword, user.password);
     if (candidateUserEmail === user.email &&
       hashCheck) {
-      res.cookie("userId", user.id);
+      // res.cookie("userId", user.id);
+      req.session.userId = user.id;
       res.redirect("/urls");
     } else {
       res.status(403).send("Your email or password was incorrect");
@@ -204,7 +201,8 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("userId");
+  // res.clearCookie("userId");
+  req.session = null;
   res.redirect("/urls");
 });
 
